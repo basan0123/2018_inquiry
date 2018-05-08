7d7cc34b05b668549f50dabcd267a34671fd23c7
basan <basanpanthin007@gmail.com>
2018-05-08 18:51:56 +0900

    first commit.

---
+++ public/form.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<from actino="./input.php" method ="post">
+お名前:<br> <input name ="name"><br>
+
+連絡(email)<br> <input name="adress"><br>
+お問い合わせ内容
+<textarea name ="body"> </textarea><br>
+<button type ="submit"> お問い合わせる</button>
+</from>
